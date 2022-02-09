09a7417935a82f5e95b42cb09df54aded59d45ff
Fernando Pereyra <fpereyra@inxap.com>
2022-02-09 17:34:35 -0300

    agrega estadistica anual

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use App\Models\Cliente;
 use App\Models\Auto;
 use App\Models\User;
 use App\Models\Venta;
+use Illuminate\Support\Facades\DB;
 
 
 use Illuminate\Http\Request;
@@ -39,10 +40,108 @@ class DashboardController extends Controller
         $totalVentasMensual = number_format($ventasMensual->sum('precio_venta'), 2, ",", ".");
         $totalGananciaMensual = number_format($ventasMensual->sum('precio_ganancia'), 2, ",", ".");
 
+        //Estadistica mensuales compras
+        $ventasMensualesCosto = Venta::whereMonth('created_at', $mes)->get();
+        $totalCostoMensual = number_format($ventasMensual->sum('precio_costo'), 2, ",", ".");
 
+        $cantidad = $mes;
+
+        //$users = DB::select('select * from users ');
+        //$users = DB::select('select * from ventas where created_at >= date_sub(now(), interval 6 month); ');
+        /*$ventasPorMeses = DB::select("SELECT
+                                sum( CASE MONTH ( a.created_at ) WHEN '1' THEN a.precio_venta ELSE 0 END ) AS enero,
+                                sum( CASE MONTH ( a.created_at ) WHEN '2' THEN a.precio_venta ELSE 0 END ) AS febrero,
+                                sum( CASE MONTH ( a.created_at ) WHEN '3' THEN a.precio_venta ELSE 0 END ) AS marzo,
+                                sum( CASE MONTH ( a.created_at ) WHEN '4' THEN a.precio_venta ELSE 0 END ) AS abril,
+                                sum( CASE MONTH ( a.created_at ) WHEN '5' THEN a.precio_venta ELSE 0 END ) AS mayo,
+                                sum( CASE MONTH (a.created_at ) WHEN '6' THEN a.precio_venta ELSE 0 END ) AS junio,
+                                sum( CASE MONTH ( a.created_at ) WHEN '7' THEN a.precio_venta ELSE 0 END ) AS julio,
+                                sum( CASE MONTH ( a.created_at ) WHEN '8' THEN a.precio_venta ELSE 0 END ) AS agosto,
+                                sum( CASE MONTH ( a.created_at ) WHEN '9' THEN a.precio_venta ELSE 0 END ) AS septiembre,
+                                sum( CASE MONTH ( a.created_at ) WHEN '10' THEN a.precio_venta ELSE 0 END ) AS octubre,
+                                sum( CASE MONTH ( a.created_at ) WHEN '11' THEN a.precio_venta ELSE 0 END ) AS noviembre,
+                                sum( CASE MONTH ( a.created_at ) WHEN '12' THEN a.precio_venta ELSE 0 END ) AS diciembre
+                            FROM
+                                ventas AS a
+                            WHERE
+                                1 = 1
+                                AND DATE_FORMAT( a.created_at, '%Y' ) = 2022;");*/
+
+
+        $ventasPorMeses = DB::select("SELECT
+
+                                sum( CASE MONTH ( a.created_at ) WHEN '1' THEN a.precio_venta ELSE 0 END ) AS enero,
+                                sum( CASE MONTH ( a.created_at ) WHEN '2' THEN a.precio_venta ELSE 0 END ) AS febrero,
+                                sum( CASE MONTH ( a.created_at ) WHEN '3' THEN a.precio_venta ELSE 0 END ) AS marzo,
+                                sum( CASE MONTH ( a.created_at ) WHEN '4' THEN a.precio_venta ELSE 0 END ) AS abril,
+                                sum( CASE MONTH ( a.created_at ) WHEN '5' THEN a.precio_venta ELSE 0 END ) AS mayo,
+                                sum( CASE MONTH (a.created_at ) WHEN '6' THEN a.precio_venta ELSE 0 END ) AS junio,
+                                sum( CASE MONTH ( a.created_at ) WHEN '7' THEN a.precio_venta ELSE 0 END ) AS julio,
+                                sum( CASE MONTH ( a.created_at ) WHEN '8' THEN a.precio_venta ELSE 0 END ) AS agosto,
+                                sum( CASE MONTH ( a.created_at ) WHEN '9' THEN a.precio_venta ELSE 0 END ) AS septiembre,
+                                sum( CASE MONTH ( a.created_at ) WHEN '10' THEN a.precio_venta ELSE 0 END ) AS octubre,
+                                sum( CASE MONTH ( a.created_at ) WHEN '11' THEN a.precio_venta ELSE 0 END ) AS noviembre,
+                                sum( CASE MONTH ( a.created_at ) WHEN '12' THEN a.precio_venta ELSE 0 END ) AS diciembre
+                            FROM
+                                ventas AS a
+                            WHERE
+                                1 = 1
+                                AND DATE_FORMAT( a.created_at, '%Y' ) = 2022;");
+
+        //return $ventasPorMeses['0'];
+        //return ($ventasPorMeses['0']);
+
+
+
+        $año = $hoy->format('Y');
+
+        $v1 = Venta::whereMonth('created_at', '01')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v2 = Venta::whereMonth('created_at', '02')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v3 = Venta::whereMonth('created_at', '03')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v4 = Venta::whereMonth('created_at', '04')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v5 = Venta::whereMonth('created_at', '05')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v6 = Venta::whereMonth('created_at', '06')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v7 = Venta::whereMonth('created_at', '07')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v8 = Venta::whereMonth('created_at', '08')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v9 = Venta::whereMonth('created_at', '09')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v10 = Venta::whereMonth('created_at', '10')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v11 = Venta::whereMonth('created_at', '11')->whereYear('created_at', '=', $año)->sum('precio_venta');
+        $v12 = Venta::whereMonth('created_at', '12')->whereYear('created_at', '=', $año)->sum('precio_venta');
+
+        $c1 = Venta::whereMonth('created_at', '01')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c2 = Venta::whereMonth('created_at', '02')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c3 = Venta::whereMonth('created_at', '03')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c4 = Venta::whereMonth('created_at', '04')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c5 = Venta::whereMonth('created_at', '05')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c6 = Venta::whereMonth('created_at', '06')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c7 = Venta::whereMonth('created_at', '07')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c8 = Venta::whereMonth('created_at', '08')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c9 = Venta::whereMonth('created_at', '09')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c10 = Venta::whereMonth('created_at', '10')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c11 = Venta::whereMonth('created_at', '11')->whereYear('created_at', '=', $año)->sum('precio_costo');
+        $c12 = Venta::whereMonth('created_at', '12')->whereYear('created_at', '=', $año)->sum('precio_costo');
+
+        $g1 = Venta::whereMonth('created_at', '01')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g2 = Venta::whereMonth('created_at', '02')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g3 = Venta::whereMonth('created_at', '03')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g4 = Venta::whereMonth('created_at', '04')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g5 = Venta::whereMonth('created_at', '05')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g6 = Venta::whereMonth('created_at', '06')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g7 = Venta::whereMonth('created_at', '07')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g8 = Venta::whereMonth('created_at', '08')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g9 = Venta::whereMonth('created_at', '09')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g10 = Venta::whereMonth('created_at', '10')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g11 = Venta::whereMonth('created_at', '11')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+        $g12 = Venta::whereMonth('created_at', '12')->whereYear('created_at', '=', $año)->sum('precio_ganancia');
+
+
+        //$vvv = $ventasPorMeses['0'];
 
         //return $autosParados;
         //die();
-        return view('admin/dashboard', compact('clientes', 'autos', 'usuarios','autosParados','totalCostoMensual','totalVentasMensual','totalGananciaMensual'));
+        return view('admin/dashboard', compact('clientes', 'autos', 'usuarios','autosParados','totalCostoMensual','totalVentasMensual','totalGananciaMensual',
+        'v1','v2','v3','v4','v5','v6','v7','v8','v9','v10','v11','v12',
+        'c1','c2','c3','c4','c5','c6','c7','c8','c9','c10','c11','c12',
+        'g1','g2','g3','g4','g5','g6','g7','g8','g9','g10','g11','g12'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,7 +11,7 @@
     <h5 class="mb-2 mt-3">Estadística mensual</h5>
     <div class="row">
         <div class="col-md-3 col-sm-6 col-12">
-            <div class="info-box" style="border-right: 2px solid #007bff">
+            <div class="info-box shadow" style="border-right: 2px solid #007bff">
                 <span class="info-box-icon p-2 "><img src="{{ asset('/imagenes/Compras.png') }}" alt=""></span>
 
                 <div class="info-box-content text-center">
@@ -24,7 +24,7 @@
         </div>
         <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-12">
-            <div class="info-box" style="border-right: 2px solid #007bff">
+            <div class="info-box shadow" style="border-right: 2px solid #007bff">
                 <span class="info-box-icon p-2"><img src="{{ asset('/imagenes/Ventas.png') }}" alt=""></span>
 
                 <div class="info-box-content text-center">
@@ -37,7 +37,7 @@
         </div>
         <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-12">
-            <div class="info-box" style="border-right: 2px solid #007bff">
+            <div class="info-box shadow" style="border-right: 2px solid #007bff">
                 <span class="info-box-icon p-2"><img src="{{ asset('/imagenes/Ganancias.png') }}" alt=""></span>
 
                 <div class="info-box-content text-center">
@@ -53,11 +53,12 @@
 
         <!-- /.col -->
     </div>
+
     <h5 class="mb-2 mt-5">Módulos Generales</h5>
     <div class="row ">
         <div class="col-lg-3 col-6">
             <!-- small box -->
-            <div class="small-box" style="border-right: 2px solid #007bff">
+            <div class="small-box shadow bg-white" style="border-right: 2px solid #007bff">
                 <div class="inner">
                     <h3>{{ count($clientes) }}</h3>
 
@@ -66,14 +67,14 @@
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="{{ route('clientes.index') }}" class="small-box-footer bg-primary">Más info <i
-                        class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('clientes.index') }}" class="small-box-footer bg-primary" style="color:white !important;">Más info <i
+                        class="fas fa-arrow-circle-right text-white"></i></a>
             </div>
         </div>
         <!-- ./col -->
         <div class="col-lg-3 col-6">
             <!-- small box -->
-            <div class="small-box" style="border-right: 2px solid #007bff">
+            <div class="small-box shadow bg-white text-white" style="border-right: 2px solid #007bff">
                 <div class="inner">
                     <h3>{{ count($autos) }}</h3>
 
@@ -82,14 +83,14 @@
                 <div class="icon">
                     <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="{{ route('autos.index') }}" class="small-box-footer bg-primary">Más info <i
-                        class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('autos.index') }}" class="small-box-footer bg-primary" style="color:white !important;">Más info <i
+                        class="fas fa-arrow-circle-right text-white"></i></a>
             </div>
         </div>
         <!-- ./col -->
         <div class="col-lg-3 col-6">
             <!-- small box -->
-            <div class="small-box " style="border-right: 2px solid #007bff">
+            <div class="small-box shadow bg-white text-white " style="border-right: 2px solid #007bff">
                 <div class="inner">
                     <h3>{{ count($usuarios) }}</h3>
 
@@ -98,8 +99,8 @@
                 <div class="icon">
                     <i class="ion ion-person-add"></i>
                 </div>
-                <a href="{{ route('users.index') }}" class="small-box-footer bg-primary">Más info <i
-                        class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('users.index') }}" class="small-box-footer bg-primary" style="color:white !important;">Más info <i
+                        class="fas fa-arrow-circle-right text-white"></i></a>
             </div>
         </div>
     </div>
@@ -108,7 +109,7 @@
 
         <!-- /.col -->
         <div class="col-md-6">
-            <div class="card card-primary">
+            <div class="card card-primary shadow bg-white">
                 <div class="card-header">
                     <h3 class="card-title">Stock parado</h3>
 
@@ -144,6 +145,92 @@
 
     </div>
 
+    <div class="row my-5">
+
+        <div class="col-4">
+            <div class="card card-primary card-outline shadow">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        <i class="far fa-chart-bar"></i>
+                        Compras
+                    </h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+
+                    <div id="compras" style="height: 300px; padding: 0px; position: relative;">
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card card-primary card-outline shadow">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        <i class="far fa-chart-bar"></i>
+                        Ventas
+                    </h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+
+                    <div id="placeholder" style="height: 300px; padding: 0px; position: relative;">
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card card-primary card-outline shadow">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        <i class="far fa-chart-bar"></i>
+                        Ganancia
+                    </h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+
+                    <div id="ganancia" style="height: 300px; padding: 0px; position: relative;">
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
 
 
 
@@ -153,4 +240,126 @@
 @stop
 
 @section('js')
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flot/4.2.2/jquery.canvaswrapper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flot/4.2.2/jquery.colorhelpers.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flot/4.2.2/jquery.flot.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flot/4.2.2/jquery.flot.saturated.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flot/4.2.2/jquery.flot.browser.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flot/4.2.2/jquery.flot.drawSeries.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flot/4.2.2/jquery.flot.uiConstants.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flot/4.2.2/jquery.flot.categories.min.js"></script>
+
+    <script type="text/javascript">
+        $(function() {
+
+            var data = [
+                ["Ene", {{ $v1 }}],
+                ["Feb", {{ $v2 }}],
+                ["Mar", {{ $v3 }}],
+                ["Abr", {{ $v4 }}],
+                ["May", {{ $v5 }}],
+                ["Jun", {{ $v6 }}],
+                ["Jul", {{ $v7 }}],
+                ["Ago", {{ $v8 }}],
+                ["Sep", {{ $v9 }}],
+                ["Oct", {{ $v10 }}],
+                ["Nov", {{ $v11 }}],
+                ["Dic", {{ $v12 }}]
+            ];
+
+            $.plot("#placeholder", [data], {
+                series: {
+                    bars: {
+                        show: true,
+                        barWidth: 0.6,
+                        align: "center"
+                    }
+                },
+                xaxis: {
+                    mode: "categories",
+                    showTicks: false,
+                    gridLines: false
+                }
+            });
+
+
+        });
+    </script>
+
+    <script type="text/javascript">
+        $(function() {
+
+            var data = [
+                ["Ene", {{ $c1 }}],
+                ["Feb", {{ $c2 }}],
+                ["Mar", {{ $c3 }}],
+                ["Abr", {{ $c4 }}],
+                ["May", {{ $c5 }}],
+                ["Jun", {{ $c6 }}],
+                ["Jul", {{ $c7 }}],
+                ["Ago", {{ $c8 }}],
+                ["Sep", {{ $c9 }}],
+                ["Oct", {{ $c10 }}],
+                ["Nov", {{ $c11 }}],
+                ["Dic", {{ $c12 }}]
+            ];
+
+            $.plot("#compras", [data], {
+                series: {
+                    bars: {
+                        show: true,
+                        barWidth: 0.6,
+                        align: "center"
+                    }
+                },
+                xaxis: {
+                    mode: "categories",
+                    showTicks: false,
+                    gridLines: false
+                }
+            });
+
+
+        });
+    </script>
+
+    <script type="text/javascript">
+        $(function() {
+
+            var data = [
+                ["Ene", {{ $g1 }}],
+                ["Feb", {{ $g2 }}],
+                ["Mar", {{ $g3 }}],
+                ["Abr", {{ $g4 }}],
+                ["May", {{ $g5 }}],
+                ["Jun", {{ $g6 }}],
+                ["Jul", {{ $g7 }}],
+                ["Ago", {{ $g8 }}],
+                ["Sep", {{ $g9 }}],
+                ["Oct", {{ $g10 }}],
+                ["Nov", {{ $g11 }}],
+                ["Dic", {{ $g12 }}]
+            ];
+
+            $.plot("#ganancia", [data], {
+                series: {
+                    bars: {
+                        show: true,
+                        barWidth: 0.6,
+                        align: "center"
+                    }
+                },
+                xaxis: {
+                    mode: "categories",
+                    showTicks: false,
+                    gridLines: false
+                }
+            });
+
+
+        });
+    </script>
 @stop
